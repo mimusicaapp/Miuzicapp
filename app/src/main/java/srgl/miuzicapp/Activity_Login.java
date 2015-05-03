7e921d507061e62da1af2240654e50fd16ad8959
LUIS <ventas@movilhuejutla.com.mx>
2015-05-03 18:50:50 -0500

    Integrando Facebook

--- app/src/main/java/srgl/miuzicapp/Activity_Login.java
+++ app/src/main/java/srgl/miuzicapp/Activity_Login.java
@@ -2,11 +2,9 @@ package srgl.miuzicapp;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.widget.EditText;
 
-public class Activity_Login extends ActionBarActivity implements TextWatcher{
+public class Activity_Login extends ActionBarActivity{
 
     private EditText email, password;
 
@@ -15,31 +13,10 @@ public class Activity_Login extends ActionBarActivity implements TextWatcher{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        //Hacemos referencia
+        //Hacemos referencia a los objetos XML
         email = (EditText) findViewById(R.id.editText_login_email);
         password = (EditText) findViewById(R.id.editText_login_password);
 
-        //Agregamos escuchadores
-        email.addTextChangedListener(this);
-        password.addTextChangedListener(this);
     }
 
-    @Override
-    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
-
-    @Override
-    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
-
-    @Override
-    public void afterTextChanged(Editable s) {
-
-        if(s.toString().length() > 0) {
-            email.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
-            password.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
-        } else {
-            //Asignar la imagen nuevamente a la vista, o cuando la cantidad de caracteres sea igual a 0
-            email.setCompoundDrawablesWithIntrinsicBounds(R.mipmap.ic_login_email, 0, 0, 0);
-            password.setCompoundDrawablesWithIntrinsicBounds(R.mipmap.ic_login_password, 0, 0, 0);
-        }
-    }
 }
