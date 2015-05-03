17c58e8d54c969e9b6fc00fcede75f0349811889
LUIS <ventas@movilhuejutla.com.mx>
2015-05-03 14:04:27 -0500

    Tema FullScreen

--- app/src/main/java/srgl/miuzicapp/Activity_SplashScreen.java
+++ app/src/main/java/srgl/miuzicapp/Activity_SplashScreen.java
@@ -1,15 +1,30 @@
 package srgl.miuzicapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Activity_SplashScreen extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_splash_screen);
+
+        TimerTask tarea = new TimerTask() {
+            @Override
+            public void run() {
+                Intent start = new Intent(getBaseContext(), Activity_Login.class);
+                startActivity(start);
+                finish();
+            }
+        };
+        Timer tiempo = new Timer();
+        tiempo.schedule(tarea, 3000);
+
     }
 
 }
